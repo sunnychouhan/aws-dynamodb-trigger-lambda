70beebd0d4f65d10830f1657ef8ba3d234a35c78
sunny <sunny.bikersedge@gmail.com>
2021-06-03 16:12:17 +0100

    Added aws lambda function to be auto  triggered from Dynamodb on CRUD operation

---
+++ src/main/java/com/chousun/awsdynamodbtrigger/AwsDynamodbTriggerApplication.java
@@ -0,0 +1,13 @@
+package com.chousun.awsdynamodbtrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDynamodbTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDynamodbTriggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chousun/awsdynamodbtrigger/AwsDynamodbTriggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chousun.awsdynamodbtrigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDynamodbTriggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
